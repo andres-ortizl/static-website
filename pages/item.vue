da853d6e708f2383f26ddea91eebb1f317669cd0
Andrés Ortiz <andres.ortiz@clarity.ai>
2021-03-14 21:49:03 +0100

    remove .nuxt

---
+++ pages/item.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="container">
+    <Logo/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "item"
+}
+</script>
+
+<style scoped>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+</style>
